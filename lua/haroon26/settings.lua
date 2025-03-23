ca8535a3d27e502e1537410a3b8e7075d9352748
Haroon Rasheed <haroonar32@gmail.com>
2025-03-24 01:28:53 +0500

    some config changed

--- lua/haroon26/settings.lua
+++ lua/haroon26/settings.lua
@@ -11,10 +11,15 @@ o.clipboard = "unnamedplus"
 -- search
 o.hlsearch = false
 o.is = true
+o.ic = true
 
 -- spacing
 o.tabstop = 2
 o.softtabstop = 2
 o.shiftwidth = 2
 o.expandtab = true
+
+-- editor
+o.wrap = false
+o.colorcolumn = "120"
 
